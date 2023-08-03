b49d4a7950e3ae322712ad35a0adfba2d8a05fbb
Soohyun Lim <d2axasap@gmail.com>
2023-08-03 15:28:31 -0400

    Modify pm2 configuration and run 4 processes only (free tier)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  apps : [{
-    name   : "server",
-    script : "./server.js",
-    instances: "-1",
+  apps: [{
+    name: "server",
+    script: "./server.js",
+    instances: "4",
     exec_mode: "cluster",
     max_memory_restart: '500M',
-    kill_timeout : 3000,
+    kill_timeout: 3000,
     wait_ready: true,
     listen_timeout: 10000,
     shutdown_with_message: true,
     env_production: {
       NODE_ENV: "production"
-   },
-   env_development: {
+    },
+    env_development: {
       NODE_ENV: "development"
-   }
+    }
   }]
 };
