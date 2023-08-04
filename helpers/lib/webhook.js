f0de2a259aaa72baae0cae571b62c468160d5f0a
Soohyun Lim <d2axasap@gmail.com>
2023-08-04 15:55:09 -0400

    Prevent the app from making duplicate webhook.

--- helpers/lib/webhook.js
+++ helpers/lib/webhook.js
@@ -4,23 +4,25 @@ const { listActiveWebhooks, createNewWebhook, deleteWebhook } = require('../../t
 const { tunnelDomain } = require('../../config/config');
 
 exports.resetWebhooks = async () => {
-  const webhooks = await listActiveWebhooks();
-
-  // collect incorrect webhooks
   const tunnelUrl = `${tunnelDomain}/notification`;
+  
+  const webhooks = await listActiveWebhooks();
 
   const invalidWebhooks = webhooks.filter(({ target }) => target !== tunnelUrl);
 
-  // delete incorrect webhooks
   const deleteWebhooks = await axios.all(
     invalidWebhooks.map(
       async ({ id }) => await deleteWebhook(id)
     )
   );
 
-  const addWebhooks = await axios.all(
-    ACUITY_EVENTS.map(
-      async (event) => await createNewWebhook(event, tunnelUrl)
-    )
-  );
+  const validWebhooks = webhooks.filter(({ target }) => target === tunnelUrl);
+
+  if (validWebhooks.length !== ACUITY_EVENTS.length) {
+    const addWebhooks = await axios.all(
+      ACUITY_EVENTS.map(
+        async (event) => await createNewWebhook(event, tunnelUrl)
+      )
+    );
+  }
 };
